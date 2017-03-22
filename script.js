2f22edd4bd843f891bff2f20100fb65fb672f883
Joel Bravo <jbravo@aclanyc.com>
2017-03-22 12:32:10 +0000

    parameter in function

--- script.js
+++ script.js
@@ -11,8 +11,30 @@ forward(50);
 right(90);
 forward(50);
 }
-
+function triangle(length , angle){
+right(angle);
+forward(length);
+right(angle);
+forward(length);
+right(angle);
+forward(length);
+}
 // Type your function call below
+function house(){
+square();
+triangle();
+}
+function rectangle(size){
+forward(size);
+right(90);
+forward(size*2);
+right(90);
+forward(size);
+right(90);
+forward(size*2);
 
+}
 
+rectangle(90);
+triangle(30, 120);
 stamp();
